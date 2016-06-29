5e36b105d40fdd034a82b77b77c3cf5de5b84877
cleardo <cleardo@yeah.net>
2016-06-29 10:15:42 +0800

    设置首页索引

--- src/main/java/com/websystique/springmvc/controller/HelloWorldRestController.java
+++ src/main/java/com/websystique/springmvc/controller/HelloWorldRestController.java
@@ -1,5 +1,6 @@
 package com.websystique.springmvc.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,16 +11,18 @@ import com.websystique.springmvc.domain.Message;
 public class HelloWorldRestController {
 
 	@RequestMapping("/")
-	public String welcome() {//Welcome page, non-rest
+	public String welcome() {
+	    // Welcome page, non-rest
 		return "Welcome to RestTemplate Example.";
 	}
 
-	@RequestMapping("/hello/{player}")
-	public Message message(@PathVariable String player) {//REST Endpoint.
+	@RequestMapping(value = "/hello/{player}", produces = MediaType.APPLICATION_JSON_VALUE)
+//	@RequestMapping("/hello/{player}")
+	public Message message(@PathVariable String player) {
 
+	    // REST Endpoint.
 		Message msg = new Message(player, "Hello " + player);
+
 		return msg;
 	}
-
-	
 }
