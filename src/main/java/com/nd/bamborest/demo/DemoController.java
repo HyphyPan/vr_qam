9849e8fddf86b8098cf1945c63442f2456c3533b
DeepOcean <773714937@qq.com>
2016-07-27 18:02:13 +0800

    潘海珲：添加cs服务，修改spring映射问题

--- src/main/java/com/nd/bamborest/demo/DemoController.java
+++ src/main/java/com/nd/bamborest/demo/DemoController.java
@@ -2,6 +2,7 @@ package com.nd.bamborest.demo;
 
 import org.springframework.http.MediaType;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
